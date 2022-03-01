a08070ab39f3df7b4559bfbbf16ac99eda4195f6
phuocphamtran <99777202+jackpham63@users.noreply.github.com>
2022-03-01 09:01:18 +0700

    code signin

--- store/index.js
+++ store/index.js
@@ -1,73 +1,17 @@
-import $fireModule from "firebase/compat";
-import { getAuth, updateProfile } from "firebase/auth";
+
 const actions = {
   async signUpUser({commit}, payload) {
-    $fireModule.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-      .then((response)=>{
-       /* newUser=user
-        console.log("oke oke ", user)
-        const auth = getAuth();*/
-        $fireModule.auth().currentUser.updateProfile({
-          displayName: payload.name,
+    $fireModule.auth()
+      .createUserWithEmailAndPassword(payload.email, payload.password)
+      // .then(user =>{
+      //   console.log(data)
+      //   user.updateProfile({displayName: payload,name})
 
-        })
-          .then(   ()=>{
-            const currentUser = {
-              id : response.uid ,
-              email: payload.email,
-              name: payload.name,
-          }
-        })
-          .then((oke)=>{
-
-          const data = {
-            email: email,
-            fullName: fullName,
-            photoURL: photoURL,
-            uid: uid,
-          };
-          $fireModule.database()
-            .ref('groups')
-            .push(userData)
-        })
       })
-/*/!*    updateProfile(auth.currentUser, {
-      displayName: payload.name
-    }).then(() => {
-      const currentUser = {
-        id : newUser.uid ,
-        email: payload.email,
-        name: payload.name,
-      }
-
-      console.log("user", currentUser)
-      commit('setUser', currentUser)
-    })
-      .then( () => {
-        const userData = {
-          email: payload.email,
-          name: payload.name,
-          createdAt: new Date().toISOString()
-        }
-        console.log(userData)
-          $fireModule.database().ref("add").push(userData)
-/!*        $fireModule.database().ref("add").push(currentUser)*!/}*!/
-
-      )*/
-      /*   .then(()=>{
-           const userData = {
-             email: payload.email,
-             name: payload.name,
-             createdAt: new Date().toISOString()
-           }
-           $fireModule.database().ref(`users/${newUser.uid}`).set(userData).then(r => {
-
-           })
-         })*/
-      .catch((error) => {
-
-      });
-
+      commit('setUser')
+      .then(() => {
+        this.$router.push("/me");
+      })
   },
   async onAuthStateChangedAction(state, { authUser, claims }) {
     if (!authUser) {
